5ed64afa9941a2ebcdb128ef442533fec6b1123f
Eric Harrer <info@eric-harrer.de>
2023-09-14 21:44:15 +0200

    [TASK] Update extension description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Partner-Rating',
-    'description' => 'Partner Rating TYPO3 Extension',
+    'description' => 'Extbase/Fluid extension that allows departments to rate collaboration partners via a front-end form. The rating is based on school grades. Grades greater than 4 must be justified. This is possible either by predefined reasons or a free text input.',
     'category' => 'plugin',
     'author' => '',
     'author_email' => '',

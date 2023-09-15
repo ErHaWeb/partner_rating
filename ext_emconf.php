2baba1da699b4acb483f8142269c21f22a4892e7
Eric Harrer <info@eric-harrer.de>
2023-09-15 10:19:06 +0200

    [TASK] Add information about the PHP version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,6 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
+            'php' => '8.2.0-8.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],

234df1e014bf3f337ed6ab4aa19400a4aef09b91
Eric Harrer <info@eric-harrer.de>
2024-02-05 16:49:19 +0100

    [TASK] Additions to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of the "partner_rating" Extension for TYPO3 CMS.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Partner Rating',
     'description' => 'Extbase/Fluid extension that allows departments to rate collaboration partners via a front-end form. Rating values can be specified as a comma separated list of integer values. Rating values greater than a configurable limit value must be justified. This is possible either by predefined reasons or a free text input.',
@@ -8,14 +21,13 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@eric-harrer.de, info@fiz-soft.de',
     'author_company' => 'eric-harrer.de, fiz-soft.de',
     'state' => 'stable',
+    'clearCacheOnLoad' => true,
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
             'php' => '7.4.0-8.2.99',
         ],
-        'conflicts' => [],
-        'suggests' => [],
     ],
     'autoload' => [
         'psr-4' => [

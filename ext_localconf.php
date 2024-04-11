b87b87a7996ad7a5e4e2ed9bcfaa903bc556cf09
Eric Harrer <info@eric-harrer.de>
2024-04-11 09:28:12 +0200

    [BUGFIX] Include page.tsconfig in TYPO3 version < 12

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,9 @@
 <?php
 
 use ErHaWeb\PartnerRating\Controller\RatingController;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
@@ -15,3 +18,11 @@ ExtensionUtility::configurePlugin(
     // non-cacheable actions
     [RatingController::class => 'list,show']
 );
+
+$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+if ($versionInformation->getMajorVersion() < 12) {
+    ExtensionManagementUtility::addPageTSConfig('
+      @import "EXT:partner_rating/Configuration/page.tsconfig"
+   ');
+}

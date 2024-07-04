ddbb99ebd1f2ea686a82b950a5fd7757682efb7d
Eric Harrer <info@eric-harrer.de>
2024-07-04 14:50:13 +0200

    [TASK] Move TypoScript files to Site Set directory

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -5,31 +5,10 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') || die();
 
 (static function (): void {
-    /**
-     * Extension key
-     */
-    $extKey = 'partner_rating';
-
-    /**
-     * TypoScript path
-     */
-    $path = 'Configuration/TypoScript';
-
-    /**
-     * Locallang file path
-     */
-    $locallangFilePath = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf';
-
-    /**
-     * Static TypoScript include title
-     */
-    $title = $locallangFilePath . ':sys_template.TypoScript.' . $extKey . '_title';
-
-    /**
-     * Add static TypoScript (constants and setup) directly through TCA instead of the API function to be able to translate the title
-     */
-    if (is_array($GLOBALS['TCA']['sys_template']['columns'])) {
-        $value = str_replace(',', '', 'EXT:' . $extKey . '/' . $path);
-        $GLOBALS['TCA']['sys_template']['columns']['include_static_file']['config']['items'][] = [$title, $value];
-    }
+    $extensionKey = 'partner_rating';
+    ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript',
+        'Partner Rating',
+    );
 })();
